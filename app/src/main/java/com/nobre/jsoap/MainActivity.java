7389e58ecf0d663962eab3f8dc70f571a946f003
nobre14 <rafanobre14@hotmail.com>
2019-11-19 16:37:53 -0300

    JSoup

---
+++ app/src/main/java/com/nobre/jsoap/MainActivity.java
@@ -0,0 +1,53 @@
+package com.nobre.jsoap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       new Thread(new BuscaCotacoesRunnable()).start();
+
+    }
+
+    public class BuscaCotacoesRunnable implements Runnable{
+
+        @Override
+        public void run() {
+            Document doc;
+
+            //https://www.bloomberg.com/quote/PETRT35:BZ
+            try {
+                doc = Jsoup.connect("https://economia.uol.com.br/cotacoes/bolsas/bvsp-bovespa/?indice=.BVSP").get();
+                Log.d("titulo", "titulo = " + doc.title());
+                Elements trs = doc.select("td.up");
+                Log.d("lista", "tamanho da lista = " + trs.text());
+
+
+
+                for (Element tr : trs) {
+
+                    Log.d("teste", "option = " + tr.text());
+                    /*Elements tds = tr.select("td.text-left");
+                    for(Element td : tds){
+                        Log.d("td", "Item td = " + td.text());
+                    }*/
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
